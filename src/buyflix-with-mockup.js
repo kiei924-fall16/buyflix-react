bd15b955c2aa1197344dcf80c4551d81a701ebcd
Brian Eng <brian@brianeng.com>
2016-10-18 16:12:44 -0500

    start with mockup

--- src/buyflix-with-mockup.js
+++ src/buyflix-with-mockup.js
@@ -32,7 +32,7 @@ var App = React.createClass({
             <div className="row">
               <div className="col-sm-2">
                 <div className="thumbnail">
-                  <img className="img-responsive" src="http://ia.media-imdb.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg" />
+                  <img className="img-responsive" role="presentation" src="http://ia.media-imdb.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg" />
                   <div className="caption">
                     <h3>Star Wars: The Force Awakens</h3>
                     <p>Action, Adventure, Fantasy</p>
@@ -42,7 +42,7 @@ var App = React.createClass({
               </div>
               <div className="col-sm-2">
                 <div className="thumbnail">
-                  <a href="#"><img className="img-responsive" src="http://ia.media-imdb.com/images/M/MV5BMjIxNDkzOTAyNV5BMl5BanBnXkFtZTgwNjEyOTY3NjE@._V1_SX300.jpg" /></a>
+                  <a href="#"><img className="img-responsive" role="presentation" src="http://ia.media-imdb.com/images/M/MV5BMjIxNDkzOTAyNV5BMl5BanBnXkFtZTgwNjEyOTY3NjE@._V1_SX300.jpg" /></a>
                   <div className="caption">
                     <h3>Point Break</h3>
                     <p>Action, Crime, Sport</p>
@@ -52,7 +52,7 @@ var App = React.createClass({
               </div>
               <div className="col-sm-2">
                 <div className="thumbnail">
-                  <a href="#"><img className="img-responsive" src="http://ia.media-imdb.com/images/M/MV5BMjM2MTQ2MzcxOF5BMl5BanBnXkFtZTgwNzE4NTUyNzE@._V1_SX300.jpg" /></a>
+                  <a href="#"><img className="img-responsive" role="presentation" src="http://ia.media-imdb.com/images/M/MV5BMjM2MTQ2MzcxOF5BMl5BanBnXkFtZTgwNzE4NTUyNzE@._V1_SX300.jpg" /></a>
                   <div className="caption">
                     <h3>The Big Short</h3>
                     <p>Biography, Drama</p>
@@ -62,7 +62,7 @@ var App = React.createClass({
               </div>
               <div className="col-sm-2">
                 <div className="thumbnail">
-                  <a href="#"><img className="img-responsive" src="http://ia.media-imdb.com/images/M/MV5BMTQ0OTE1MTk4N15BMl5BanBnXkFtZTgwMDM5OTk5NjE@._V1_SX300.jpg" /></a>
+                  <a href="#"><img className="img-responsive" role="presentation" src="http://ia.media-imdb.com/images/M/MV5BMTQ0OTE1MTk4N15BMl5BanBnXkFtZTgwMDM5OTk5NjE@._V1_SX300.jpg" /></a>
                   <div className="caption">
                     <h3>Daddy's Home</h3>
                     <p>Comedy</p>
@@ -72,7 +72,7 @@ var App = React.createClass({
               </div>
               <div className="col-sm-2">
                 <div className="thumbnail">
-                  <a href="#"><img className="img-responsive" src="http://ia.media-imdb.com/images/M/MV5BMjIxOTI0MjU5NV5BMl5BanBnXkFtZTgwNzM4OTk4NTE@._V1_SX300.jpg" /></a>
+                  <a href="#"><img className="img-responsive" role="presentation" src="http://ia.media-imdb.com/images/M/MV5BMjIxOTI0MjU5NV5BMl5BanBnXkFtZTgwNzM4OTk4NTE@._V1_SX300.jpg" /></a>
                   <div className="caption">
                     <h3>Bridge of Spies</h3>
                     <p>Biography, Drama</p>
@@ -82,7 +82,7 @@ var App = React.createClass({
               </div>
               <div className="col-sm-2">
                 <div className="thumbnail">
-                  <a href="#"><img className="img-responsive" src="http://ia.media-imdb.com/images/M/MV5BMTA3Mzc5Nzk0ODReQTJeQWpwZ15BbWU4MDA2ODQxMTcx._V1_SX300.jpg" /></a>
+                  <a href="#"><img className="img-responsive" role="presentation" src="http://ia.media-imdb.com/images/M/MV5BMTA3Mzc5Nzk0ODReQTJeQWpwZ15BbWU4MDA2ODQxMTcx._V1_SX300.jpg" /></a>
                   <div className="caption">
                     <h3>Life</h3>
                     <p>Biography, Drama</p>
@@ -92,7 +92,7 @@ var App = React.createClass({
               </div>
               <div className="col-sm-2">
                 <div className="thumbnail">
-                  <a href="#"><img className="img-responsive" src="http://ia.media-imdb.com/images/M/MV5BODg5NDM1MDI4NF5BMl5BanBnXkFtZTgwMzg0MzQxNzE@._V1_SX300.jpg" /></a>
+                  <a href="#"><img className="img-responsive" role="presentation" src="http://ia.media-imdb.com/images/M/MV5BODg5NDM1MDI4NF5BMl5BanBnXkFtZTgwMzg0MzQxNzE@._V1_SX300.jpg" /></a>
                   <div className="caption">
                     <h3>Creed</h3>
                     <p>Drama, Sport</p>
@@ -102,7 +102,7 @@ var App = React.createClass({
               </div>
               <div className="col-sm-2">
                 <div className="thumbnail">
-                  <a href="#"><img className="img-responsive" src="http://ia.media-imdb.com/images/M/MV5BMjM2Nzg4MzkwOF5BMl5BanBnXkFtZTgwNzA0OTE3NjE@._V1_SX300.jpg" /></a>
+                  <a href="#"><img className="img-responsive" role="presentation" src="http://ia.media-imdb.com/images/M/MV5BMjM2Nzg4MzkwOF5BMl5BanBnXkFtZTgwNzA0OTE3NjE@._V1_SX300.jpg" /></a>
                   <div className="caption">
                     <h3>Spectre</h3>
                     <p>Action, Adventure, Thriller</p>
@@ -112,7 +112,7 @@ var App = React.createClass({
               </div>
               <div className="col-sm-2">
                 <div className="thumbnail">
-                  <a href="#"><img className="img-responsive" src="http://ia.media-imdb.com/images/M/MV5BNjEzNTk2OTEwNF5BMl5BanBnXkFtZTgwNzExMTg0NjE@._V1_SX300.jpg" /></a>
+                  <a href="#"><img className="img-responsive" role="presentation" src="http://ia.media-imdb.com/images/M/MV5BNjEzNTk2OTEwNF5BMl5BanBnXkFtZTgwNzExMTg0NjE@._V1_SX300.jpg" /></a>
                   <div className="caption">
                     <h3>Burnt</h3>
                     <p>Comedy, Drama</p>
@@ -122,7 +122,7 @@ var App = React.createClass({
               </div>
               <div className="col-sm-2">
                 <div className="thumbnail">
-                  <a href="#"><img className="img-responsive" src="http://ia.media-imdb.com/images/M/MV5BMTc2MTQ3MDA1Nl5BMl5BanBnXkFtZTgwODA3OTI4NjE@._V1_SX300.jpg" /></a>
+                  <a href="#"><img className="img-responsive" role="presentation" src="http://ia.media-imdb.com/images/M/MV5BMTc2MTQ3MDA1Nl5BMl5BanBnXkFtZTgwODA3OTI4NjE@._V1_SX300.jpg" /></a>
                   <div className="caption">
                     <h3>The Martian</h3>
                     <p>Adventure, Comedy, Drama</p>
@@ -132,7 +132,7 @@ var App = React.createClass({
               </div>
               <div className="col-sm-2">
                 <div className="thumbnail">
-                  <a href="#"><img className="img-responsive" src="http://ia.media-imdb.com/images/M/MV5BMTA5MzkyMzIxNjJeQTJeQWpwZ15BbWU4MDU0MDk0OTUx._V1_SX300.jpg" /></a>
+                  <a href="#"><img className="img-responsive" role="presentation" src="http://ia.media-imdb.com/images/M/MV5BMTA5MzkyMzIxNjJeQTJeQWpwZ15BbWU4MDU0MDk0OTUx._V1_SX300.jpg" /></a>
                   <div className="caption">
                     <h3>Straight Outta Compton</h3>
                     <p>Biography, Drama</p>
@@ -142,7 +142,7 @@ var App = React.createClass({
               </div>
               <div className="col-sm-2">
                 <div className="thumbnail">
-                  <a href="#"><img className="img-responsive" src="http://ia.media-imdb.com/images/M/MV5BMTUyNjE5NjI5OF5BMl5BanBnXkFtZTgwNzYzMzU3NjE@._V1_SX300.jpg" /></a>
+                  <a href="#"><img className="img-responsive" role="presentation" src="http://ia.media-imdb.com/images/M/MV5BMTUyNjE5NjI5OF5BMl5BanBnXkFtZTgwNzYzMzU3NjE@._V1_SX300.jpg" /></a>
                   <div className="caption">
                     <h3>The Intern</h3>
                     <p>Comedy</p>
@@ -155,7 +155,7 @@ var App = React.createClass({
           <div className="details col-sm-4">
             <div className="row">
               <div className="col-sm-6">
-                <img className="poster img-responsive" src="http://ia.media-imdb.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg" />
+                <img className="poster img-responsive" role="presentation" src="http://ia.media-imdb.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg" />
               </div>
               <div className="col-sm-6">
                 <h3>Star Wars: The Force Awakens</h3>
